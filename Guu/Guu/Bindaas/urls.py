e830d4e81acd4c8aba3b013d5f7c7801031ae9c9
Aagam Shah <69311837+AAGAM17@users.noreply.github.com>
2025-02-22 02:49:54 +0530

    Update Bindaas app URLs and views with new feature routes and templates

--- Guu/Guu/Bindaas/urls.py
+++ Guu/Guu/Bindaas/urls.py
@@ -3,6 +3,12 @@ from Bindaas import views
 
 
 urlpatterns = [
-    path('Bindaas/', views.bindaas_view, name='Bindaas'),
+    path('', views.home, name='home'),
+    path('speech-to-text/', views.speech_to_text, name='speech_to_text'),
+    path('gesture-recognition/', views.gesture_recognition, name='gesture_recognition'),
+    path('chatbot/', views.chatbot, name='chatbot'),
+    path('video-tutorials/', views.video_tutorials, name='video_tutorials'),
+    path('gestures/', views.gestures, name='gestures'),
+    path('text-to-speech/', views.text_to_speech, name='text_to_speech'),
 ]
 
--- Guu/Guu/Bindaas/views.py
+++ Guu/Guu/Bindaas/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import json
 
 def landing(request):
     return render(request, "landing.html")
 
 def gestures(request):
-    return render(request, "Gestures.html")
+    return render(request, "gestures.html")
 
 def home(request):
     return render(request, "home.html")
+
+def speech_to_text(request):
+    return render(request, "speech_to_text.html")
+
+def gesture_recognition(request):
+    return render(request, "gesture_recognition.html")
+
+def chatbot(request):
+    return render(request, "chatbot.html")
+
+def video_tutorials(request):
+    return render(request, "video_tutorials.html")
+
+def text_to_speech(request):
+    return render(request, "text_to_speech.html")
