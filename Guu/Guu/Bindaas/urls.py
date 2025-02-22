7ebd3dfebd2e6c7d514316865a83ae6f15f3dd55
deepdas2607 <157793903+deepdas2607@users.noreply.github.com>
2025-02-22 07:49:40 +0530

    made all features working except 1

--- Guu/Guu/Bindaas/urls.py
+++ Guu/Guu/Bindaas/urls.py
@@ -4,11 +4,11 @@ from Bindaas import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('speech-to-text/', views.speech_to_text, name='speech_to_text'),
-    path('gesture-recognition/', views.gesture_recognition, name='gesture_recognition'),
+    path('speech_to_text/', views.speech_to_text, name='speech_to_text'),
+    path('gesture_recognition/', views.gesture_recognition, name='gesture_recognition'),
     path('chatbot/', views.chatbot, name='chatbot'),
-    path('video-tutorials/', views.video_tutorials, name='video_tutorials'),
+    path('video_tutorials/', views.video_tutorials, name='video_tutorials'),
     path('gestures/', views.gestures, name='gestures'),
-    path('text-to-speech/', views.text_to_speech, name='text_to_speech'),
+    path('text_to_speech/', views.text_to_speech, name='text_to_speech'),
 ]
 
--- Guu/Guu/Guu/urls.py
+++ Guu/Guu/Guu/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
-from Bindaas.views import gestures, home, landing
+from Bindaas.views import gestures, home, landing, speech_to_text, text_to_speech, gesture_recognition, chatbot, video_tutorials
 from Bindaas import views
 
 urlpatterns = [ 
     path('', views.landing, name='landing'),
     path('gestures/', views.gestures, name='gestures'),
     path('home/', views.home, name='home'),
+    path('speech_to_text/', views.speech_to_text, name='speech_to_text'),
+    path('text_to_speech/', views.text_to_speech, name='text_to_speech'),
+    path('gesture_recognition/', views.gesture_recognition, name='gesture_recognition'),
+    path('chatbot/', views.chatbot, name ='chatbot'),
+    path('video_tutorials/', views.video_tutorials, name='video_tutorials'),
 ]
